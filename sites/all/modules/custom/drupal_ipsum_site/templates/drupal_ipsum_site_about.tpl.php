36f7da0261a08925faa9afa9ec50b6c9d3894462
Alex Weber <alexweber15@gmail.com>
2011-11-23 22:04:32 -0200

    drupal_ipsum_site module
    moved site-specific hooks and modifications here

---
+++ sites/all/modules/custom/drupal_ipsum_site/templates/drupal_ipsum_site_about.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file Drupal Ipsum Site about page
+ * 
+ * Displays information about Drupal Ipsum
+ * This project has no fields so we are using a template to better control the markup
+ */
+?>
+<p>Drupal Ipsum is a free service that generates Drupal-flavoured Lorem Ipsum filler text!</p>
+
+<h2>Why</h2>
+
+<p>Why not?!</p>
+
+<h2>Who</h2>
+
+<p>Drupal Ipsum was an idea of ours at <a href="http://webdrop.net.br" title="Webdrop" rel="follow" target="_blank">Webdrop</a> and was developed for fun as a side-project by <a href="http://www.alexweber.com.br" title="Alex Weber" rel="follow" target="_blank">Alex</a>.</p>
+
+<h2>Help!</h2>
+
+<p>Did we miss any classic Drupal words? Please let us know! For now we are going to have to ask you to use either one of the contact forms from the afforementioned websites.</p>
+
+<p>Thanks!</p>
