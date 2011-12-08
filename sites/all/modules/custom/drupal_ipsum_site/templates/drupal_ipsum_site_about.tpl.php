c1efa8615da1ad1a46f92505da169535751e872a
Alex Weber <alexweber15@gmail.com>
2011-12-08 02:01:36 -0200

    tweaked about msg

--- sites/all/modules/custom/drupal_ipsum_site/templates/drupal_ipsum_site_about.tpl.php
+++ sites/all/modules/custom/drupal_ipsum_site/templates/drupal_ipsum_site_about.tpl.php
@@ -16,8 +16,12 @@
 
 <p>Drupal Ipsum was an idea of ours at <a href="http://webdrop.net.br" title="Webdrop" rel="follow" target="_blank">Webdrop</a> and was developed for fun as a side-project by <a href="http://www.alexweber.com.br" title="Alex Weber" rel="follow" target="_blank">Alex</a>.</p>
 
+<h2>Get the Module!</h2>
+
+<p><a href="http://drupal.org/project/drupal_ipsum" target="_blank" title="Drupal Ipsum project page on Drupal.org">Drupal Ipsum is now available as a contrib module on Drupal.org!</a></p>
+
 <h2>Help!</h2>
 
-<p>Did we miss any classic Drupal words? Please <a href="/contact" title="Contact Us">let us know!</a></p>
+<p>Did we miss any classic Drupal words? Or include crappy non-Drupal words? Please <a href="/contact" title="Contact Us">let us know!</a></p>
 
-<p>Thanks!</p>
+<p><strong>Thanks!</strong></p>
